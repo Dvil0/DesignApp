7a10603d7538c3e85c68e0c02e3c8ab595d3b05b
Dvil0 <Dvil0@users.noreply.github.com>
2019-09-13 18:39:13 -0500

    Add final design with gradients, table, tableRows and more.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:designsapp/src/views/buttons_view.dart';
 import 'package:designsapp/src/views/scrolls_view.dart';
 import 'package:flutter/material.dart';
 import 'package:designsapp/src/views/basic_view.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,13 +11,18 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     
+    SystemChrome.setSystemUIOverlayStyle( SystemUiOverlayStyle.light.copyWith(
+      statusBarColor: Colors.transparent
+    ));
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Designs',
-      initialRoute: 'scroll',
+      initialRoute: 'button',
       routes: {
         'basic'   : ( BuildContext context ) => BasicView(),
         'scroll'  : ( BuildContext context ) => ScrollsView(),
+        'button'  : ( BuildContext context ) => ButtonsView(),
       },
     );
   }
---
+++ lib/src/views/buttons_view.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+import 'dart:ui';
+
+class ButtonsView extends StatelessWidget{
+
+  @override
+  Widget build( BuildContext context ){
+
+
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          _backgroundImage(),
+          SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                _titles(),
+                _buttons(),
+              ],
+            ),
+          )
+        ],
+      ),
+      bottomNavigationBar: _bottomNavigationBar( context )
+    );
+  }
+
+  Widget _backgroundImage(){
+
+    final gradiente = Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromRGBO(52, 54, 101, 1.0),
+            Color.fromRGBO(35, 37, 57, 1.0)
+          ],
+          begin: FractionalOffset( 0.0, 0.6),
+          end: FractionalOffset( 0.0, 1.0),
+        )
+      ),
+    );
+
+    final pinkBox = Transform.rotate(
+      angle: -pi / 5.0,
+      child: Container(
+        height: 360.0,
+        width: 360.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(80.0),
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(236, 98, 108, 1.0),
+              Color.fromRGBO(241, 142, 172, 1.0)
+            ]
+          )
+        ),
+      )
+    );
+
+    return Stack(
+      children: <Widget>[
+        gradiente,
+        Positioned(
+          top: -100,
+          child: pinkBox
+        )
+      ],
+    );
+  }
+
+  Widget _titles(){
+
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text('Classify transaction', style: TextStyle( color: Colors.white, fontSize: 30.0, fontWeight: FontWeight.bold)),
+            SizedBox(height: 10.0,),
+            Text('Classify this transaction into a particular category', style: TextStyle( color: Colors.white, fontSize: 18.0))
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _bottomNavigationBar( BuildContext context ){
+
+    return Theme(
+      data: Theme.of(context).copyWith(
+        canvasColor: Color.fromRGBO(55, 57, 84, 1.0),
+        primaryColor: Colors.pinkAccent,
+        textTheme: Theme.of(context).textTheme.copyWith(
+          caption: TextStyle( color: Color.fromRGBO(116, 117, 152, 1.0))
+        )
+      ),
+      child: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            title: Container(),
+            icon: Icon( Icons.calendar_today, size: 30.0, )
+          ),
+          BottomNavigationBarItem(
+            title: Container(),
+            icon: Icon( Icons.bubble_chart, size: 30.0, )
+          ),
+          BottomNavigationBarItem(
+            title: Container(),
+            icon: Icon( Icons.supervised_user_circle, size: 30.0, )
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buttons(){
+
+    return Table(
+      children: [
+        TableRow(
+          children: [
+            _createButton( Colors.blue, Icons.border_all, 'General' ),
+            _createButton( Colors.purple, Icons.directions, 'Bus' ),
+          ]
+        ),
+        TableRow(
+          children: [
+            _createButton( Colors.pinkAccent, Icons.shop, 'Buy' ),
+            _createButton( Colors.orange, Icons.insert_drive_file, 'File' ),
+          ]
+        ),
+        TableRow(
+          children: [
+            _createButton( Colors.blueAccent, Icons.movie_filter, 'Entertaiment' ),
+            _createButton( Colors.green, Icons.cloud, 'Grocery' ),
+          ]
+        ),
+        TableRow(
+          children: [
+            _createButton( Colors.red, Icons.collections, 'Photos' ),
+            _createButton( Colors.teal, Icons.help_outline, 'General' ),
+          ]
+        ),
+      ],
+    );
+  }
+
+  Widget _createButton(Color color, IconData icon, String text){
+
+    return ClipRect(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+        child: Container(
+          height: 180.0,
+          margin: EdgeInsets.all(15.0),
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(62, 66, 107, 0.7),
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              SizedBox( height: 5.0),
+              CircleAvatar(
+                backgroundColor: color,
+                radius: 35.0,
+                child: Icon( icon, color: Colors.white, size: 30.0,),
+              ),
+              Text( text, style: TextStyle(color: color)),
+              SizedBox( height: 5.0),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
